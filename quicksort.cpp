7c84afa201fd3439989b9de8367b29c9b5d95e73
Wahyu <firmansyahw503@gmail.com>
2023-03-30 11:24:32 +0700

    line 58 - 82

--- quicksort.cpp
+++ quicksort.cpp
@@ -54,4 +54,31 @@ void q_short(int low, int high)
 	i = low + 1;									// Langkah 3
 	j = high;										// Langkah 4
 
+
+	while (i <= j)									// Langkah 10
+	{
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))	// Langkah 5
+		{
+			i++;									// Langkah 6
+				cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))		// Langkah 7
+		{
+			j--;									// Langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+
+		if (i < j) // if the greater element is on the left of the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+
 }
